a30380eb29ecc50b6721d0fc137893b631e1271b
AndabeleGit <Andabele10@hotmail.con>
2024-10-10 09:10:10 -0500

    styles.css

--- my-app/Hello.py
+++ my-app/Hello.py
@@ -1,25 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from markupsafe import escape
+from datetime import datetime
 
 app = Flask(__name__)
 
+# filtro personalizado
+@app.add_template_filter
+def today(date):
+    return date.strftime('%d-%m-%Y')
+
+# funcion personalizada
+# manera para no enviarla por el reder template
+@app.add_template_global 
+def repeat(s, n):
+    return s * n
+
+# ruta principal
 @app.route('/')
 def index():
-    name = None
+    print(url_for('index'))
+    print(url_for('Hello'))
+    name = 'Alex'
+    date = datetime.now()
     friends = ['Ándres', 'Juan', 'Pedro']
-    return render_template('index.html', name = name, friends = friends)
-
+    return render_template('index.html', 
+                           name = name, 
+                           friends = friends, 
+                           fecha = date,
+                        )
 
 @app.route('/Hello')
 @app.route('/Hello/<name>')
 @app.route('/Hello/<name>/<int:age>')
-def Hello(name = None, age = None):
-    if name == None and age == None:
-        return "<h1>Hola Mundo</h1>"
-    elif age == None:
-        return f"<h1>Hola {name}"
-    else:
-        return f'Hola {name}, y tu edad es {age}'
+@app.route('/Hello/<name>/<int:age>/<email>')
+def Hello(name = None, age = None, email = None):
+    datos = {
+        'name': name,
+        'age': age,
+        'email': email
+    }
+    return render_template('hello.html',
+                       datos = datos)
 
 app.route('code/<path:code>')
 def code(code):
