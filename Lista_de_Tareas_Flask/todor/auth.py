02613ea74e43643038f4fae812912a80d0bf1481
AndabeleGit <Andabele10@hotmail.con>
2024-10-17 16:54:16 -0500

    finished

--- Lista_de_Tareas_Flask/todor/auth.py
+++ Lista_de_Tareas_Flask/todor/auth.py
@@ -1,9 +1,10 @@
 from flask import (
-    Blueprint, render_template, request, url_for, redirect, flash
+    Blueprint, render_template, request, url_for, redirect, flash, session, g
 )
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from todor import db
+import functools
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -28,6 +29,47 @@ def register():
 
     return render_template('auth/register.html')
 
-@bp.route('/login', methods=['GET', 'POST'])  # Puedes también manejar POST si el login necesita formulario
+@bp.route('/login', methods=['GET', 'POST'])  
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        error = None
+
+        user = User.query.filter_by(username=username).first()
+        if user == None:
+            error = "El nombre de usuario es incorrecto"
+            flash(error)
+        elif not check_password_hash(user.password, password):
+            error = "Contraseña incorrecta"
+            flash(error)
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user.id
+            return redirect(url_for('todo.index'))
+
     return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id == None:
+        g.user = None
+    else:
+        g.user = User.query.get_or_404(user_id)
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+def login_required(view): 
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view
